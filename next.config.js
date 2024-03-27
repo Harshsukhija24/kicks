aa29d01514008dc37d28da868c4b99d25564aedb
Harshsukhija24 <harshsukhija2002@gmail.com>
2024-03-28 00:28:49 +0530

    r1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["www.shutterstock.com", "res.cloudinary.com"],
+    domains: [
+      "www.shutterstock.com",
+      "res.cloudinary.com",
+      "dx8gt3nnu.image",
+      "sneakerplug.co.in",
+      "cdn.shopify.com",
+      "images.stockx.com",
+      "www.kicksmachine.com",
+      "hypefly-assets.s3.ap-south-1.amazonaws.com",
+      "image.goxip.com",
+      // Add more domains as needed
+    ],
   },
 };
 
